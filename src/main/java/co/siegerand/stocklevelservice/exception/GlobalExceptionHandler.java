023069034ac400027beb8c0ff040d2b17be0d48f
Victor <tvicky4j247@gmail.com>
2021-12-27 13:20:12 -0500

    Fixes bug in GlobalExceptionHandler that caused error handling to fail

--- src/main/java/co/siegerand/stocklevelservice/exception/GlobalExceptionHandler.java
+++ src/main/java/co/siegerand/stocklevelservice/exception/GlobalExceptionHandler.java
@@ -3,9 +3,10 @@ package co.siegerand.stocklevelservice.exception;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import co.siegerand.stocklevelservice.util.other.HttpErrorInfo;
@@ -15,12 +16,14 @@ public class GlobalExceptionHandler {
 
     private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(InvalidInputException.class)
     public @ResponseBody HttpErrorInfo handleInvalidInputException(ServerHttpRequest request, InvalidInputException ex) {
         return generateErrorInfo(HttpStatus.UNPROCESSABLE_ENTITY, request, ex);
     }
 
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
     public @ResponseBody HttpErrorInfo handleNotFoundException(ServerHttpRequest request, NotFoundException ex) {
         return generateErrorInfo(HttpStatus.NOT_FOUND, request, ex);
