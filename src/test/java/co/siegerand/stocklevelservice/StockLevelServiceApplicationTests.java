1e4dd27fb436ca2dedc0638871f0a1f1784be228
Victor <tvicky4j247@gmail.com>
2021-12-28 16:51:53 -0500

    Adds more tests for other service functions
    Adds tests for:
        - Stock replenishment (one pass and one fail)
        - Book purchase (also one pass and one fail)
    
    Modifies tests for:
        - Book deletion (removes deletion confirmation check. Might add modify and readd in the future)

--- src/test/java/co/siegerand/stocklevelservice/StockLevelServiceApplicationTests.java
+++ src/test/java/co/siegerand/stocklevelservice/StockLevelServiceApplicationTests.java
@@ -1,10 +1,15 @@
 package co.siegerand.stocklevelservice;
 
+import co.siegerand.stocklevelservice.model.BookPurchase;
 import co.siegerand.stocklevelservice.model.StockLevel;
+import co.siegerand.stocklevelservice.model.StockReplenishment;
 import co.siegerand.stocklevelservice.persistence.entity.StockLevelEntity;
 import co.siegerand.stocklevelservice.persistence.repository.BookPurchaseRepository;
 import co.siegerand.stocklevelservice.persistence.repository.StockLevelRepository;
 import co.siegerand.stocklevelservice.persistence.repository.StockReplenishmentRepository;
+
+import java.time.ZonedDateTime;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,7 +19,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
-import reactor.test.StepVerifier;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
                 properties = {"eureka.client.enabled=false"})
@@ -83,22 +87,106 @@ class StockLevelServiceApplicationTests extends MySqlDbContainer {
     }
 
     @Test
+    void purchaseBook() {
+        int bookId = 1;
+        int stockLevel = 10;
+        int booksPurchased = 5;
+        
+        // create book
+        createBookHelper(bookId, stockLevel);
+
+        // purchase book via api
+        BookPurchase bookPurchase = new BookPurchase(1, bookId, booksPurchased, ZonedDateTime.now());
+        
+        StockLevel sLevel = webTestClient.post()
+            .uri("/inventory/buy")
+            .accept(MediaType.APPLICATION_JSON)
+            .bodyValue(bookPurchase)
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody(StockLevel.class).returnResult().getResponseBody();
+    
+        Assertions.assertNotNull(sLevel);
+        Assertions.assertEquals(stockLevel - booksPurchased, sLevel.getStockLevel());
+    }
+
+    @Test
+    void purchaseBookOutOfStock() {
+        int bookId = 1;
+        int stockLevel = 3;
+        int booksPurchased = 5;
+        
+        // create book
+        createBookHelper(bookId, stockLevel);
+
+        // purchase book via api
+        BookPurchase bookPurchase = new BookPurchase(1, bookId, booksPurchased, ZonedDateTime.now());
+
+        webTestClient.post()
+            .uri("/inventory/buy")
+            .accept(MediaType.APPLICATION_JSON)
+            .bodyValue(bookPurchase)
+            .exchange()
+            .expectStatus().isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY);
+
+    }
+
+    @Test
+    void replenishStock() {
+        int bookId = 1;
+        int initialStock = 10;
+        int replenish = 10;
+
+        // create book
+        createBookHelper(bookId, initialStock);
+
+        // replenish book supply
+        StockReplenishment replenishment = new StockReplenishment(bookId, replenish, ZonedDateTime.now());
+
+        StockLevel newStockLevel = webTestClient.post()
+            .uri("/inventory/replenish")
+            .accept(MediaType.APPLICATION_JSON)
+            .bodyValue(replenishment)
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody(StockLevel.class).returnResult().getResponseBody();
+        
+        Assertions.assertNotNull(newStockLevel);
+        Assertions.assertEquals(initialStock + replenish, newStockLevel.getStockLevel());
+    }
+
+    @Test
+    void replenishStockFail() {
+        int bookId = 1;
+        int initialStock = 300;
+        long replenish = Long.MAX_VALUE;
+
+        // create book
+        createBookHelper(bookId, initialStock);
+
+        // replenish stock - expect failure
+        StockReplenishment replenishment = new StockReplenishment(bookId, replenish, ZonedDateTime.now());
+
+        webTestClient.post()
+            .uri("/inventory/replenish")
+            .accept(MediaType.APPLICATION_JSON)
+            .bodyValue(replenishment)
+            .exchange()
+            .expectStatus().isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @Test
     void deleteBook() {
         int bookId = 1;
         // create book
         createBookHelper(bookId, 10);
+        Assertions.assertTrue(stockLevelRepository.findByBookId(bookId).isPresent());
 
         // delete book
         webTestClient.delete()
             .uri("/inventory/" + bookId)
             .exchange()
             .expectStatus().isOk();
-
-        // confirm deletion
-        webTestClient.get()
-            .uri("/inventory/" + bookId)
-            .exchange()
-            .expectStatus().isEqualTo(HttpStatus.NOT_FOUND);
     }
 
     // UTIL METHODS
