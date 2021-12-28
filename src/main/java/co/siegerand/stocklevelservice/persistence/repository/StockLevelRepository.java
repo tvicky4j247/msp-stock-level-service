606b02fa1410c8ba4bcb84d1a470880f6b76c553
Victor <tvicky4j247@gmail.com>
2021-12-28 17:45:20 -0500

    Bug fix to StockLevelRepository
    Fixes a bug caused by the StockLevelRepository using the StockLevel class rather than the StockLevelEntity class

--- src/main/java/co/siegerand/stocklevelservice/persistence/repository/StockLevelRepository.java
+++ src/main/java/co/siegerand/stocklevelservice/persistence/repository/StockLevelRepository.java
@@ -5,10 +5,8 @@ import java.util.Optional;
 import co.siegerand.stocklevelservice.persistence.entity.StockLevelEntity;
 import org.springframework.data.repository.CrudRepository;
 
-import co.siegerand.stocklevelservice.model.StockLevel;
-
 public interface StockLevelRepository extends CrudRepository<StockLevelEntity, Integer> {
     
-    Optional<StockLevel> findByBookId(int bookId);
+    Optional<StockLevelEntity> findByBookId(int bookId);
 
 }
