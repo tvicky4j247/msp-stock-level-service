bb57db960bef74e256377c19918d870263e14d98
Victor <tvicky4j247@gmail.com>
2021-12-30 18:48:27 -0500

    Same as previous commit

--- src/main/java/co/siegerand/stocklevelservice/persistence/repository/BookPurchaseRepository.java
+++ src/main/java/co/siegerand/stocklevelservice/persistence/repository/BookPurchaseRepository.java
@@ -1,10 +1,12 @@
 package co.siegerand.stocklevelservice.persistence.repository;
 
 import co.siegerand.stocklevelservice.persistence.entity.BookPurchaseEntity;
+import reactor.core.publisher.Mono;
+
 import org.springframework.data.repository.CrudRepository;
 
-import co.siegerand.stocklevelservice.model.BookPurchase;
-
 public interface BookPurchaseRepository extends CrudRepository<BookPurchaseEntity, Integer> {
+
+    Mono<Void> deleteAllByBookId(int bookId);
     
 }
