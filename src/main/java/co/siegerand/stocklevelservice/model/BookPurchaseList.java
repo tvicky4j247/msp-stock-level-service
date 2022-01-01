f19e817311175aaae36645d5d919924cf782fd2e
Victor <tvicky4j247@gmail.com>
2022-01-01 08:38:33 -0500

    Adds two new model classes for new service implementation

---
+++ src/main/java/co/siegerand/stocklevelservice/model/BookPurchaseList.java
@@ -0,0 +1,13 @@
+package co.siegerand.stocklevelservice.model;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class BookPurchaseList {
+    
+    private List<BookPurchase> BookPurchaseList;
+}
---
+++ src/main/java/co/siegerand/stocklevelservice/model/StockReplenishmentList.java
@@ -0,0 +1,14 @@
+package co.siegerand.stocklevelservice.model;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class StockReplenishmentList {
+    
+    private List<StockReplenishment> stockReplenishmentList;
+
+}
